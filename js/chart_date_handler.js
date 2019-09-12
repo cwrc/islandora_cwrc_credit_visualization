55e88f4c106178605ffecf49d0a420f8a06ac359
Nia Kathoni <nikathone@gmail.com>
2019-09-12 16:55:34 +0000

    Using highchart for visualization.

---
+++ js/chart_date_handler.js
@@ -0,0 +1,98 @@
+/**
+ * @file
+ * Add the no ui customization and some date filter handler.
+ */
+
+(function ($, Drupal) {
+
+  Drupal.behaviors.cwrcCredVizChartDateHandler = {
+    attach: function (context, settings) {
+      var module_settings = settings.islandora_cwrc_credit_visualization,
+        no_ui_slider_settings = module_settings.no_ui_slider,
+        dateFilterSlider = document.getElementById(no_ui_slider_settings.placeholder_id),
+        dateFilterDisplays = [
+          document.getElementById('filter-date-range-start'),
+          document.getElementById('filter-date-range-end')
+        ],
+        dateMinInput = document.getElementsByName("date_range[min]")[0],
+        dateMaxInput = document.getElementsByName("date_range[max]")[0],
+        time_wrapper = $('.highcharts-title > tspan:nth-child(4)', context);
+
+      if (time_wrapper.length) {
+        var date = new Date(),
+          date_options = {
+            month: 'long',
+            day: 'numeric',
+            year: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            timeZoneName: 'short'
+          };
+
+        time_wrapper.html(date.toLocaleDateString([navigator.language, 'en-US'], date_options));
+      }
+
+      // Instantiate the no ui slider for date range filter.
+      noUiSlider.create(dateFilterSlider, {
+        connect: true,
+        // No closer than 1 together.
+        margin: 1,
+        // Snap to 1-unit increments.
+        step: 1,
+        range: {
+          min: timestamp(no_ui_slider_settings.range.min),
+          max: timestamp(no_ui_slider_settings.range.max)
+        },
+        start: [
+          timestamp(no_ui_slider_settings.start.begin),
+          timestamp(no_ui_slider_settings.start.end),
+        ],
+        format: wNumb({
+          decimals: 0
+        }),
+        pips: {
+          mode: 'positions',
+          values: [0, 33, 66, 100],
+          density: 1,
+          format: {
+            to: function (value) {
+              var date = new Date(value);
+              return formatDate(date);
+            },
+            from: function (value) {
+              return value.getTime();
+            }
+          }
+        }
+      });
+      dateFilterSlider.noUiSlider.on('update', function (values, handle) {
+        var value = values[handle];
+        // Update the date display for the user.
+        dateFilterDisplays[handle].innerHTML = formatDate(new Date(+value));
+        // Update hidden min and max value.
+        var value_input = value / 1000;
+        if (handle) {
+          dateMaxInput.value = value_input.toFixed(0);
+        }
+        else {
+          dateMinInput.value = value_input.toFixed(0);
+        }
+      });
+
+      /**
+       * Date formatter.
+       *
+       * @param date
+       */
+      function formatDate(date) {
+        var formattedMonth = ('0' + (date.getMonth() + 1)).slice(-2),
+          formattedDate = ('0' + date.getDate()).slice(-2);
+        return formattedMonth + '/' + formattedDate + '/' + date.getFullYear();
+      }
+
+      function timestamp(str) {
+        return new Date(str).getTime();
+      }
+    }
+  };
+})(jQuery, Drupal);
---
+++ js/checkboxes_handler.js
@@ -0,0 +1,88 @@
+/**
+ * @file
+ * Add the no ui customization and some date filter handler.
+ */
+
+(function ($, Drupal) {
+
+  Drupal.behaviors.cwrcCredVizChartCheckboxesHandler = {
+    attach: function (context, settings) {
+      var module_settings = settings.islandora_cwrc_credit_visualization,
+        users_checkboxes = '#' + module_settings.filters_wrapper_id + ' .form-item-users input',
+        selAllNoneWrapper = [$('#edit-users-select-all-toggle-wrapper', context), $('#edit-document-pids-select-all-toggle-wrapper', context)],
+        selAll = Drupal.t('Select All'),
+        selNone = Drupal.t('Select None');
+
+      $.each(selAllNoneWrapper, function (i, toggleWrapper) {
+        var allChecked = true,
+          checkboxes = $(this).closest('.form-type-checkboxes', context).find('.form-checkboxes .form-checkbox');
+
+        if ($(toggleWrapper).length) {
+          checkboxes.each(function (i, item) {
+            if (!$(item).is(':checked')) {
+              allChecked = false;
+              return;
+            }
+            $(item).on('change', function (e) {
+              var link_toggle = $(this).closest('.form-type-checkboxes', context).find('label .checkboxes-select-all-none-toggle');
+              checkboxes.not(':checked').length ? link_toggle.html(selAll) : link_toggle.html(selNone);
+            });
+          });
+          if (allChecked) {
+            $(toggleWrapper).append(getToggleLink(selNone));
+          }
+          else {
+            $(toggleWrapper).append(getToggleLink(selAll));
+          }
+        }
+      });
+
+      function filterByUsers(chart, series) {
+        var checked = [];
+        // Getting checked users.
+        $.each($(users_checkboxes, context), function (i, user) {
+          if ($(this).is(':checked')) {
+            checked.push($(this).attr('name'));
+          }
+        });
+
+        // var newCategories = [];
+        $.each(series, function (i, ser) {
+          var newData = [];
+          $.each(ser.data, function (j, point) {
+            // console.log(point);
+            var input_name = 'users[' + point.user_id + ']';
+            if ($.inArray(input_name, checked) != -1) {
+              newData.push(point);
+              // newCategories.push(point.name);
+            }
+          });
+          chart.series[ser.index].setData(newData);
+        });
+        // chart.xAxis[0].setCategories(newCategories, false);
+        // chart.redraw();
+      }
+
+      function getToggleLink(link_text) {
+        var toggleLink = $('<a class="checkboxes-select-all-none-toggle" href="#">' + link_text + '</a>'),
+          selNoneWarning = Drupal.t('You must keep at least one item selected to load more items. Deselect after loading if desired.');
+
+        toggleLink.click(function (event) {
+          // Don't actually follow the link...
+          event.preventDefault();
+          event.stopPropagation();
+
+          var noneSelected = selAll === $(this).text();
+          $(this).html(noneSelected ? selNone : selAll);
+          $(this).attr('title', noneSelected ? selNoneWarning : '');
+
+          var checkboxes = $(this).closest('.form-type-checkboxes', context).find('.form-checkboxes .form-checkbox');
+          checkboxes.each(function (index, item) {
+            $(item).prop('checked', noneSelected);
+          });
+        });
+        return toggleLink;
+      }
+    }
+  };
+})(jQuery, Drupal);
